a2e450ba60b838959dfa1e61acfbcc0f8f3f24bf
Atul Varma <varmaa@gmail.com>
2012-07-26 18:37:20 -0500

    added requireAccessToken middleware.

--- test/test-token-middleware.js
+++ test/test-token-middleware.js
@@ -1,49 +1,67 @@
 var expect = require('expect.js'),
     request = require('request'),
     express = require('express'),
-    TokenMiddleware = require('../lib/token-middleware');
+    tokenMiddleware = require('../lib/token-middleware');
 
-describe("TokenMiddleware", function() {
-  function req(method, accessToken, cb) {
-    var server = express.createServer();
-    server.use(express.bodyParser());
-    server.use(TokenMiddleware({
-      getTokenInfo: function(token, cb) {
-        expect(token).to.be(accessToken);
-        if (token == "abcd")
-          cb(null, {name: "foo"});
-        else if (token == "error")
-          cb("FAIL");
-        else
-          cb(null, null);
-      }
-    }));
-    server.get("/", function(req, res) { res.send(req.user); });
-    server.post("/", function(req, res) { res.send(req.user); });
-    server.listen(5234, function() {
-      var url = 'http://127.0.0.1:5234/';
-      
-      function finishReq(e, r, b) {
-        server.close();
-        cb(e, r, b);
-      }
-      
-      if (method == 'GET') {
-        if (accessToken)
-          url += '?accessToken=' + accessToken;
-        request(url, finishReq);
-      } else if (method == 'POST') {
-        request({
-          method: 'POST',
-          url: url,
-          form: {
-            accessToken: accessToken
-          }
-        }, finishReq);
-      }
-    });
-  }
+function req(method, accessToken, cb, constructor) {
+  var server = express.createServer();
+  server.use(express.bodyParser());
+  server.use((constructor || tokenMiddleware.accessToken)({
+    getTokenInfo: function(token, cb) {
+      expect(token).to.be(accessToken);
+      if (token == "abcd")
+        cb(null, {name: "foo"});
+      else if (token == "error")
+        cb("FAIL");
+      else
+        cb(null, null);
+    }
+  }));
+  server.get("/", function(req, res) { res.send(req.user); });
+  server.post("/", function(req, res) { res.send(req.user); });
+  server.listen(5234, function() {
+    var url = 'http://127.0.0.1:5234/';
+    
+    function finishReq(e, r, b) {
+      server.close();
+      cb(e, r, b);
+    }
+    
+    if (method == 'GET') {
+      if (accessToken)
+        url += '?accessToken=' + accessToken;
+      request(url, finishReq);
+    } else if (method == 'POST') {
+      request({
+        method: 'POST',
+        url: url,
+        form: {
+          accessToken: accessToken
+        }
+      }, finishReq);
+    }
+  });
+}
 
+describe("tokenMiddleware.requireAccessToken()", function() {
+  it("should deny requests without valid access tokens", function(done) {
+    req('GET', null, function(error, response, body) {
+      expect(response.statusCode).to.be(403);
+      expect(body).to.eql('valid access token required');
+      done();
+    }, tokenMiddleware.requireAccessToken);
+  });
+  
+  it("should accept requests with valid access tokens", function(done) {
+    req('POST', 'abcd', function(error, response, body) {
+      expect(response.statusCode).to.be(200);
+      expect(JSON.parse(body)).to.eql({name: 'foo'});
+      done();
+    }, tokenMiddleware.requireAccessToken);
+  });
+});
+
+describe("tokenMiddleware.accessToken()", function() {
   it("should work when token is passed in POST requests", function(done) {
     req('POST', 'abcd', function(error, response, body) {
       expect(response.statusCode).to.be(200);
