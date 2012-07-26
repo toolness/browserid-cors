6ac01a8d45e8d047de9a61d5f2776c4638d59a98
Atul Varma <varmaa@gmail.com>
2012-07-26 13:01:43 -0500

    improved manual test

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ var express = require('express'),
     browserid = require('./browserid'),
     app = express.createServer(),
     LocalTokenStorage = require('./local-token-storage'),
+    TokenMiddleware = require('./token-middleware'),
     lts = LocalTokenStorage();
 
 app.use(express.bodyParser());
+app.use(TokenMiddleware(lts));
 app.post('/auth', auth.handler({
   verify: browserid.verify,
   createToken: lts.createToken
 }));
+app.get('/token-info', function(req, res) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.send(req.user);
+});
 
 module.exports = app;
 
